eed010bbf03eeab1484fc4d886e85a1bb786bcf2
Brian LeQuang <brian.lequang@gmail.com>
2019-06-13 15:04:20 -0500

    learn Bootstrap

---
+++ src/views/Navbar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import '../css/Navbar.css'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <h3> Check Bootstrap introduction </h3>
+    );
+  }
+}
+
+export default Navbar;
