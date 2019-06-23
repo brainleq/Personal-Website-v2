67b0a3245ee2093ce3e5727046ee52c22b4f80ca
Brian LeQuang <brian.lequang@gmail.com>
2019-06-22 21:15:16 -0500

    landing page mostly complete. Begin work on aboutme.

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,16 +1,52 @@
 import React, { Component } from 'react';
 
 import '../css/page.css';
+import github from '../images/icons/github.png'
+import facebook from '../images/icons/facebook.png'
+import linkedin from '../images/icons/linkedIn.png'
+import email from '../images/icons/email.png'
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <div class="homeImg"></div>
-        <h2>Read more</h2>
-        <h2>Read more</h2>
-        <h2>Read more</h2>
-        <h2>Read more</h2>
+
+        <div class="homeImg">
+          <div class="center landing">
+            <div class="frontTitle"><h1>BRIAN LEQUANG</h1></div>
+            <h4>a student at the University of Texas at Austin with a passion for innovative technology</h4>
+          </div>
+          <div class="links">
+            <ul id="in-line">
+              <li>
+                <a href="https://github.com/brainleq"><img src={github} id="icon"></img></a>
+              </li>
+              <li>
+                <a href="https://www.facebook.com/brian.lequang"><img src={facebook} id="icon"></img></a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/brian-lequang-200bb9182/"><img src={linkedin} id="icon"></img></a>
+              </li>
+              <li>
+                <a href="/contact"><img src={email} id="icon"></img></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="dark-back extra-space">
+          <div class="white landing container">
+            <h3 id="letter-spacing">ABOUT ME</h3>
+            <p id="aboutPad">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+            in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </div>
+
       </div>
     );
   }
--- src/views/Navbar.js
+++ src/views/Navbar.js
@@ -4,27 +4,43 @@ import { Nav } from 'react-bootstrap';
 import '../css/Navbar.css'
 
 class Navbar extends Component {
+
+  componentDidMount () {
+    var prevPosition = window.pageYOffset;
+    var nav_element = document.getElementById("nav");
+    var navItems_element = document.getElementById("projects");
+    window.onscroll = function() {
+      var currentPosition = window.pageYOffset;
+      if (currentPosition <= 0) {
+        // If not scrolled
+        nav_element.style.top = "0";
+        nav_element.style.background = "";
+      } else {
+        nav_element.style.top = "-75px";
+        //nav_element.style.background = "white";
+      }
+      prevPosition = currentPosition;
+    }
+  }
+
   render() {
     return (
-      <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="nav">
-        <a class="navbar-brand" href="/">Brian LeQuang</a>
+      <nav class="navbar navbar-expand-lg fixed-top" id="nav">
+            <div class="container">
+        <a class="navbar-brand scrollN" href="/">Brian LeQuang</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-           <li className="nav-item">
-             <a className="nav-link" id="projects" href="/projects">Projects</a>
-           </li>
+          <ul className="navbar-nav mr-auto" id="nav-items">
+           <li><a className="nav-link scrollN" id="projects" href="/projects">Projects</a></li>
           </ul>
           <ul class="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" id="contact" href="/contact">Contact</a>
-            </li>
+            <li><a className="nav-link scrollN" id="contact" href="/contact">Contact</a></li>
           </ul>
         </div>
-
+            </div>
       </nav>
     );
   }
