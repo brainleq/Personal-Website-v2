39555cdf315c01f722542a30dd76fa3548d6d967
Brian <brian.lequang@gmail.com>
2020-12-22 14:49:13 -0600

    fade-in animation and lazyload images

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 
-import '../css/contact.css';
-import SocialLinks from '../components/SocialLinks.js';
+import "../css/contact.css";
+import "../css/page.css"
+import SocialLinks from "../components/SocialLinks.js";
 class Contact extends Component {
 
   render() {
     return(
-      <div id="topPad">
+      <div className="fade-in" id="topPad">
         <div className="extraTopSpace">
-          <div className="container" style={{position:'relative'}}>
+          <div className="container" style={{position:"relative"}}>
             <div className="container formPad formText formWidth">
-              <h1 id="letter-spacing" style={{paddingBottom: '35px'}}>STAY CONNECTED</h1>
-              <p>Feel free to get in touch. You can reach me at these social networking sites
-              or send me an email </p>
+              <h1 id="letter-spacing" style={{paddingBottom: "35px"}}>STAY CONNECTED</h1>
+              <p style={{paddingBottom: "20px"}}>
+                Feel free to get in touch. You can reach me at these social networking sites
+                or send me an email
+              </p>
               <SocialLinks/>
             </div>
           </div>
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,28 +1,33 @@
 import React, { Component } from "react";
-
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/opacity.css';
 import "../css/page.css";
+
 import SocialLinks from "../components/SocialLinks.js";
 import Hotjava from "../images/hotjava.png"
+
 
 class Home extends Component {
   render() {
     return (
-      <div>
-
-        <div class="homeImg">
-          <div class="center landing">
-            <div class="frontTitle"><h1 class="landingTitleGap">BRIAN LEQUANG</h1></div>
-            <h4 class="landingTitleGap">a student at The University of Texas at Austin with a passion for innovative technology</h4>
+      <div className="dark-back">
+        <div className="homeImg fade-in">
+          <div className="fade-in-slow">
+            <div className="center landing">
+              <div className="frontTitle"><h1 className="landingTitleGap">BRIAN LEQUANG</h1></div>
+              <h4 className="landingTitleGap">a student at The University of Texas at Austin with a passion for innovative technology</h4>
+            </div>
+            <SocialLinks/>
           </div>
-          <SocialLinks/>
         </div>
 
-        <div class="dark-back extra-space">
-          <div class="row white landing">
-            <div class="col-md-4 aboutImg">
-              <img src={Hotjava} alt="hotjava" height="563" width="375" />
+        <div className="dark-back extra-space fade-in">
+          <div className="row white landing">
+            <div className="col-md-4 aboutImg">
+              <LazyLoadImage effect="opacity" src={Hotjava} height="536" width="375" />
+              {/*<img src={Hotjava} alt="hotjava" height="563" width="375" />*/}
             </div>
-            <div class="col-md-8 extra-space">
+            <div className="col-md-8 extra-space">
               <h1 id="letter-spacing">WELCOME</h1>
               <p id="aboutPad">I am a machine learning enthusiast, a
                 hobbyist programmer, and a computer science student at
